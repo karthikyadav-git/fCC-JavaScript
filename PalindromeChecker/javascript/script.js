914c557050ae2107df69edf9bf17ccf68e3f159a
Karthik Yadav <mail@karthikyadav.com>
2025-01-01 16:34:32 +0545

    added palindrome check script

--- PalindromeChecker/javascript/script.js
+++ PalindromeChecker/javascript/script.js
@@ -1,1 +1,30 @@
 // select elements
+const input = document.getElementById('text-input');
+const checkButton = document.getElementById('check-btn');
+const result = document.getElementById('result');
+
+// convert input to alphanumeric
+const toAlphanumeric = (inputString) => {
+    return inputString.replace(/[^a-zA-Z0-9]/g, '');
+};
+
+// string reversal function
+const reverse = (normal) => {
+    return normal.split('').reverse().join('');
+};
+
+// check event
+checkButton.addEventListener("click", 
+    () => {
+        alphanum = toAlphanumeric(input.value.toLowerCase());
+        let output;
+        if (alphanum === reverse(alphanum)) {
+            output = `${input.value} is a palindrome.`;
+        }
+        else {
+            output = `${input.value} is not a palindrome.`;
+        }
+        result.innerHTML = output;
+        input.value = '';
+    }
+);
